d3dc68ca7a659d72e64e452c17ccafda5ee92f74
Tobias <verleger.t@gmail.com>
2020-11-29 21:16:53 +0100

    byte

--- ImageReader/ImageReader.py
+++ ImageReader/ImageReader.py
@@ -6,29 +6,34 @@ import io
 
 def processImage(path: str) -> None:
     image=readFile(path)
-    resizedImage=resizeImage(image)
+    resizedImage: Image=resizeImage(image)
     byteArr=convertToBytArray(resizedImage)
     writeFileAsByteArray(byteArr)
 
 
 def readFile(path: str)-> Image:
     image = Image.open(path, mode='r')
+    print(type(image))
+
     return image
 
 def convertToBytArray(image: Image)-> str:
     img_byte_arr = io.BytesIO()
+    print(type(image))
 
     image.save(img_byte_arr, format='PNG')
-    img_byte_arr = img_byte_arr.getvalue()
+    img_byte_arr = img_byte_arr.getvalue()  
     return img_byte_arr
 
 def writeFileAsByteArray(input: str) -> None: 
-    f = open("bytearray.txt", "w")
-    f.write(input)
+    f = open("bytearray.txt", "wb")
+    byteString = bytearray(input)
+    f.write( str(byteString.decode() ))
     f.close()
+  
 
 
 def resizeImage(image: Image)->Image: 
     size = (800, 600)
     fit_and_resized_image = ImageOps.fit(image, size, Image.ANTIALIAS)
-    return fit_and_resized_image
+    return fit_and_resized_image 
