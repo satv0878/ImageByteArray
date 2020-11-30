5a58ecd0b55f8e6ea18e8fc12cfc523a53d601ce
Tobias <verleger.t@gmail.com>
2020-11-30 20:32:48 +0100

    array

--- ImageReader/ImageReader.py
+++ ImageReader/ImageReader.py
@@ -1,18 +1,19 @@
 
 from PIL import Image, ImageOps
-
+import base64
 import io
 
 
 def processImage(path: str) -> None:
     image=readFile(path)
-    resizedImage: Image=resizeImage(image)
-    byteArr=convertToBytArray(resizedImage)
+    resizedImage: Image = resizeImage(image)
+    resizedImage.show()
+    byteArr = convertToBytArray(resizedImage)
     writeFileAsByteArray(byteArr)
 
 
 def readFile(path: str)-> Image:
-    image = Image.open(path, mode='r')
+    image = Image.open(path, mode='r').convert('LA')
     print(type(image))
 
     return image
@@ -26,14 +27,17 @@ def convertToBytArray(image: Image)-> str:
     return img_byte_arr
 
 def writeFileAsByteArray(input: str) -> None: 
-    f = open("bytearray.txt", "wb")
+    f = open("bytearray.txt", "w")
     byteString = bytearray(input)
-    f.write( str(byteString.decode() ))
+    for i in input:
+        f.write(hex(i)+',')
+    
+    
     f.close()
   
 
 
 def resizeImage(image: Image)->Image: 
-    size = (800, 600)
-    fit_and_resized_image = ImageOps.fit(image, size, Image.ANTIALIAS)
+    size = (400, 300)
+    fit_and_resized_image = ImageOps.fit(image, size, Image.BILINEAR)
     return fit_and_resized_image 
